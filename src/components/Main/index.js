9731ad5ad8f1ddd23ed51db2ad362510a3ab3e72
Dusan Pantelic <dusan1717@hotmail.com>
2018-05-28 19:04:02 +0200

    first commit

---
+++ src/components/Main/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Series from '../../Containers/Series/';
+import SingleSeries from '../../Containers/SingleSeries';
+
+const Main = props => {
+    return (
+        <Switch>
+            <Route exact path="/" component={Series} />
+            <Route path="/series/:id" component={SingleSeries}/>
+        </Switch>
+    )
+};
+
+export default Main;
